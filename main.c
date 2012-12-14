5d9a817e645f012976dd38ea72b114909a0704c6
makestuff <github.com@m3.ath.cx>
2012-12-14 11:46:16 +0000

    Completed port to libusb-1.0

--- main.c
+++ main.c
@@ -1,39 +1,33 @@
 #include <libusbwrap.h>
-#ifdef WIN32
-	#include <lusb0_usb.h>
-#else
-	#include <usb.h>
-#endif
 #include <liberror.h>
 #include <stdio.h>
 
 int main(int argc, const char *argv[]) {
-	int returnCode;
-	struct usb_dev_handle *device = NULL;
+	int returnCode = 0;
+	struct USBDevice *device = NULL;
 	const char *error = NULL;
-	uint16 vid, pid;
-	usbInitialise();
-	if ( argc != 3 ) {
-		fprintf(stderr, "Synopsis: %s <vid> <pid>\n", argv[0]);
-		FAIL(1);
-	}
-	vid = (uint16)strtoul(argv[1], NULL, 16);
-	pid = (uint16)strtoul(argv[2], NULL, 16);
-	if ( usbOpenDevice(vid, pid, 1, 0, 0, &device, &error) ) {
+	const char *vp;
+	if ( argc != 2 ) {
+		fprintf(stderr, "Synopsis: %s <VID:PID>\n", argv[0]);
 		FAIL(2);
 	}
-	if ( usbPrintConfiguration(device, stdout, &error) ) {
+	if ( usbInitialise(0, &error) ) {
+		FAIL(1);
+	}
+	vp = argv[1];
+	if ( usbOpenDevice(vp, 1, 0, 0, &device, &error) ) {
 		FAIL(3);
 	}
-	returnCode = 0;
+	if ( usbPrintConfiguration(device, stdout, &error) ) {
+		FAIL(4);
+	}
 cleanup:
 	if ( device ) {
-		usb_release_interface(device, 0);
-		usb_close(device);
+		usbCloseDevice(device, 0);
 	}
 	if ( error ) {
 		fprintf(stderr, "%s: %s\n", argv[0], error);
 		errFree(error);
 	}
-	return 0;
+	return returnCode;
 }
